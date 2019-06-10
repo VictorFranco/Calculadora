42a83ed692daabce4d9b4b596398ef7bc2d0ae1f
Victor Franco <vfrancohdez1@hotmail.com>
2019-06-10 12:31:54 -0500

    actualizacion

---
+++ Calculadora/Calculadora.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int num1, num2, resultado;
+int main() {
+	cout << "hola soy una calculadora" << endl;
+	system("pause");
+	return 0;
+}
--- Calculadora/Source.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-	cout << "hola mundo" << endl;
-	system("pause");
-}
